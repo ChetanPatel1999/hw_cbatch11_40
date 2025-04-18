325be7bef84081d48a463fe35952512e26717413
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-04-18 12:39:49 +0530

    18/04

---
+++ 01_first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	printf("hello \nstudents\n");
+	printf("how \t\tare you ? ");
+	printf("\nwhats about your parents ?");	
+}
+
---
+++ 02_comment.c
@@ -0,0 +1,12 @@
+//wap to print your introduction.
+#include<stdio.h> // stdio.h is a library/header file
+void main()  //we write all code of c lanaguge inside main function
+{
+	printf("hello world institute\n");  // printf is built-in function
+   //	printf("this is second c code\n");
+	printf("this code write in dev cpp");
+}
+/*this is
+multiline comment
+using this we can 
+comment more than one line*/

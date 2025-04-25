35efd90a57d7d92bd240b9729bd49da37452da5b
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-04-25 12:56:23 +0530

    04/25

--- 03_integer_formate_specifire.c
+++ 03_integer_formate_specifire.c
@@ -3,7 +3,7 @@
 void main()
 {
 	printf("integer value = %d\n",12);
-	printf("integer value = %d\n",3+5);
+	printf("integer value = %d\n",37%5);
 	printf("integer value = %d\n",12-9);
 	printf("integer value = %d\n",12*9);
 	printf("frist value = %d and secode value = %d",56,90);
--- 18_airthmetic.c
+++ 18_airthmetic.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    printf("hello student how are you");
+    int a, b, ans;
+    printf("enter frist value : ");
+    scanf("%d",&a);
+    printf("enter second value : ");
+    scanf("%d",&b);
+    ans = a + b;
+    printf("addition : %d\n", ans);//38
+    ans = a - b;
+    printf("subtraction : %d\n", ans);//30
+    ans = a * b;
+    printf("multiplication : %d\n", ans);//136
+    ans = a / b;
+    printf("division : %d\n", ans);//7
+    ans = a % b;
+    printf("modules : %d\n", ans);//7
 }
